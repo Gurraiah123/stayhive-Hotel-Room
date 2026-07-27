e976c7647916060f50042df303ccf2c071e93d36
Premchand-96 <narsingpremchand@gmail.com>
2026-07-27 14:40:31 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  host: process.env.DB_HOST || '40.192.0.73',
+  user: process.env.DB_USER || 'stayhive',
+  password: process.env.DB_PASSWORD || 'StayHive@123',
   database: process.env.DB_NAME || 'stayhive',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
